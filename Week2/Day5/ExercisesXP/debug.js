7c54a8f45c82b7f194967d50dc02f71ae9b5a646
lemi-dev <l.f.weizmann@gmail.com>
2022-10-24 02:17:50 +0300

    added day 4 mandatory exercises

---
+++ Week2/Day5/ExercisesXP/debug.js
@@ -0,0 +1,3 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+confirm("Would you like to play a game?");
---
+++ Week2/Day5/ExercisesXP/mini-project-play-the-game-script.js
@@ -0,0 +1,29 @@
+function onlyNumbers(str) {
+  const regex = new RegExp(/^[0-9]*$/);
+  return regex.test(str);
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+let confirmToPlay;
+
+let numberGuess;
+
+function playTheGame() {
+  confirm("Would you like to play a game?");
+
+  confirmToPlay
+    ? (numberGuess = prompt("Enter a number from 0 to 10"))
+    : alert("No problem, Goodbye");
+
+  if (numberGuess !== Number(numberGuess)) {
+    alert("Sorry not a Number, Goodbye");
+  } else if (numberGuess < 0 || numberGuess > 10) {
+    alert("Sorry not a good number, Goodbye");
+  } else {
+  }
+}
