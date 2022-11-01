98ce3e18ce2544b61447597e6d9935a32f35abab
lemi-dev <l.f.weizmann@gmail.com>
2022-11-01 12:54:10 +0200

    week4 day2

--- Week4/Day2/XP/Mandatory/4/script.js
+++ Week4/Day2/XP/Mandatory/4/script.js
@@ -7,3 +7,17 @@ John has just signed in to your website and you want to welcome him.
     In your js file, create a self invoking funtion that takes 1 argument: the name of the user that just signed in.
     The function should add a div in the nabvar, displaying the name of the user and his profile picture.
 */
+
+(function () {
+  const paragraph = document.createElement("p");
+  paragraph.innertext = "Ben";
+
+  const image = document.createElement("img");
+  image.src =
+    "https://i.picsum.photos/id/377/200/200.jpg?hmac=rcJQMvK0zOsZkiVu3ncmb8j3LjiGAySY2-TqaHP5jnw";
+
+  const navbar = document.querySelector(".navbar");
+
+  navbar.appendChild(paragraph);
+  navbar.appendChild(image);
+})();
--- Week4/Day2/XP/Mandatory/5/script.js
+++ Week4/Day2/XP/Mandatory/5/script.js
@@ -1,20 +1,48 @@
-const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
-const ordinal = ["th", "st", "nd", "rd"];
-
-let sentence = colors.forEach((item, index) => {
-  let order;
-  switch (index) {
-    case 0:
-      order = ordinal[1];
-      break;
-    case 1:
-      order = ordinal[2];
-      break;
-    case 2:
-      order = ordinal[3];
-      break;
-    default:
-      order = ordinal[0];
+//Part I
+/*
+function makeJuice(sizeOfBeverage) {
+  function addIngredients(ingredient1, ingredient2, ingredient3) {
+    return console.log(
+      `The client wants a ${sizeOfBeverage} juice, containing ${ingredient1}, ${ingredient2}, ${ingredient3}`
+    );
   }
-  console.log(`${index + 1}${order} choice is ${item}`);
-});
+  addIngredients("orange", "lemon", "sugar");
+}
+
+makeJuice("large");
+*/
+//Part II
+
+function makeJuice(sizeOfBeverage) {
+  let ingredients = [];
+  const firstPart = `The client wants a ${sizeOfBeverage} juice, containing `;
+  let secondPart = "";
+
+  function addIngredients(ingredient1, ingredient2, ingredient3) {
+    ingredients.push(ingredient1);
+    ingredients.push(ingredient2);
+    ingredients.push(ingredient3);
+  }
+
+  addIngredients("orange", "lemon", "sugar");
+  addIngredients("orange", "lemon", "sugar");
+
+  function displayJuice(ingredients) {
+    const isLastElement = (array, index) => index === array.length - 1;
+    ingredients.forEach(
+      (igredient, index) =>
+        (secondPart += `${igredient}${
+          isLastElement(ingredients, index) ? "." : ", "
+        }`)
+    );
+    const sentence = firstPart + secondPart;
+    console.log(sentence);
+
+    const element = document.createElement("p");
+    element.innerText = sentence;
+    document.body.appendChild(element);
+  }
+  displayJuice(ingredients);
+}
+
+makeJuice("large");
