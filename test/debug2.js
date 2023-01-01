562282596cefb1c1bbcad547f668c0dbf1690ee6
lemi-dev <l.f.weizmann@gmail.com>
2023-01-01 21:21:46 +0200

    add: week9 day4 xp mandatory

--- test/debug2.js
+++ test/debug2.js
@@ -1,19 +1,34 @@
-let number = 4;
+// Exercise 1 : Giphy API
+// Instructions
+// With your knewly accumulated knowledge of AJAX lets write some cool code!
 
-function testNum(number) {}
+// You will work with this part of the documention
 
-const proms = new Promise((resolve, reject) => {
-  if (number < 10) {
-    resolve("it is smaller than 10");
+// You will use this Gif URL: https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My
+// Explanation of the Gif URL and the queries
+
+// q Request Parameter: Search query term or phrase. Above, the URL is searching for “hilarious” gifs
+
+// rating Request Parameter: Filters results by specified rating. We are searching for Level 1 gifs. Check out the ratings documentation
+
+// api_key Request Paramater : GIPHY API Key. Our API KEY is hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My
+
+// Create a program to retrieve the data from the API URL provided above.
+//Use XMLHttpRequest object to make an AJAX request to the Giphy API and console.log the Javascript Object.
+
+let xhr = new XMLHttpRequest();
+
+xhr.open(
+  "GET",
+  "https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My"
+);
+
+xhr.send();
+
+xhr.onload = () => {
+  if (xhr.status != 200) {
+    alert(`Error ${xhr.status}, ${xhr.statusText}`);
   } else {
-    reject("the condition failed");
+    console.log(`Done ${xhr.response}`);
   }
-});
-
-proms
-  .then((result) => {
-    console.log(result);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+};
